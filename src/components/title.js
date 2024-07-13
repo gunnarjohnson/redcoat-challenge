78ff9f881132fd747b51c2498b99366a51ff0bff
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2024-07-13 17:19:21 -0500

    Add Title component

---
+++ src/components/title.js
@@ -0,0 +1,41 @@
+import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const TitleWrapper = styled.span`
+  font-family: "patua one", sans-serif;
+  font-size: 1.2em;
+  font-weight: 400;
+  text-transform: uppercase;
+`
+
+const Title = ({ year }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  const { title } = data.site.siteMetadata
+
+  return (
+    <TitleWrapper>
+      {title}
+      {year !== "" ? ` ${year}` : ""}
+    </TitleWrapper>
+  )
+}
+
+Title.propTypes = {
+  year: PropTypes.string,
+}
+
+Title.defaultProps = {
+  year: "",
+}
+
+export default Title
